b96312e8ba26272f625772f496404189fc10debb
Vlad Ciubotariu <vlad.ciubotariu@centric.eu>
2022-07-13 09:58:01 +0300

    Added DIP fix

--- SOLID/DependencyInversionPrinciple/Violation/Program.cs
+++ SOLID/DependencyInversionPrinciple/Violation/Program.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using DependencyInversionPrinciple.Violation.Classes;
-
-namespace DependencyInversionPrinciple.Violation
+﻿namespace DependencyInversionPrinciple.Violation
 {
+    //DIP violated
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            UserService userService = new UserService();
-            User[] users = userService.GetAllUsers();
-            foreach (User user in users)
-            {
-                Console.WriteLine($"Username: {user.Username}; Email: {user.Email};");
-            }
-        }
+        // public static void Main(string[] args)
+        // {
+        //     UserService userService = new UserService();
+        //     User[] users = userService.GetAllUsers();
+        //     foreach (User user in users)
+        //     {
+        //         Console.WriteLine($"Username: {user.Username}; Email: {user.Email};");
+        //     }
+        // }
     }
 }
